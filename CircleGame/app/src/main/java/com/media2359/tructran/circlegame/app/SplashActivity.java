4c5f8a6d7ae917408082bf132e71863511cacb7e
Truc Tran Trung <truc.trantrung@2359media.com>
2016-05-15 09:57:32 +0700

    Create project

---
+++ CircleGame/app/src/main/java/com/media2359/tructran/circlegame/app/SplashActivity.java
@@ -0,0 +1,13 @@
+package com.media2359.tructran.circlegame.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
